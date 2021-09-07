fc72cc2f10eccb1f2b04a0bc4b38ce615a372b74
Cesar Camarena <cesar.camarena@smoothstack.com>
2021-09-07 15:36:57 -0500

    First Assignment of Smoothstack Java Basics.

---
+++ assignment1/src/com/ss/jb/one/Assignment1.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.ss.jb.one;
+
+/**
+ * @author Cesar Camarena
+ *
+ */
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		System.out.println("1)");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println(".........");
+		System.out.println("2)");
+		System.out.println("..........");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+		System.out.println("3)");
+		System.out.println("     *");
+		System.out.println("    ***");
+		System.out.println("   *****");
+		System.out.println("  *******");
+		System.out.println("...........");
+		System.out.println("4)");
+		System.out.println("............");
+		System.out.println("  *******");
+		System.out.println("   *****");
+		System.out.println("    ***");
+		System.out.println("     *");
+		
+	}
+
+}
