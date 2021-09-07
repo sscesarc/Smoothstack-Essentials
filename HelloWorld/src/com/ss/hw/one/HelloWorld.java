df6df45abbff33bcb84ebdb65d7477bab024326b
Cesar Camarena <cesar.camarena@smoothstack.com>
2021-09-07 15:12:46 -0500

    Testing git with eclipse

---
+++ HelloWorld/src/com/ss/hw/one/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.hw.one;
+
+/**
+ * @author Cesar Camarena
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+
+}
